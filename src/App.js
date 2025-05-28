502ef1519c806906969be4cdfc8301af2b93b0cf
Blerta Aliu <blertaaliu6@gmail.com>
2025-05-28 16:01:51 +0200

    Remove emailjs

--- src/App.js
+++ src/App.js
@@ -4,13 +4,8 @@ import Banner from './components/Banner';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Footer from './components/Footer';
-import emailjs from '@emailjs/browser';
 
 function App() {
-  useEffect(() => {
-    emailjs.init("YOUR_PUBLIC_KEY"); // Replace with your public key
-  }, []);
-
   return (
     <div className="App">
       <Banner />
